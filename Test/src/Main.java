40a45651ddc9ca5fec5e13fee0c97ea254993c1d
Svyatoslav Kotov <kotov.svyat@mail.ru>
2022-01-22 19:22:49 +0300

    test

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Good");
+
+        System.out.println("vsjklnlsjkf");
+        System.out.println("");
     }
 }
